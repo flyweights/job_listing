a94bf66e781c35a2b5f1467dc9d2b49d263ebe7b
flyweights <flyweights@163.com>
2020-07-17 09:37:50 +0800

    implement job CRUD

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,41 @@
+class JobsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create update edit destroy]
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to jobs_ptah
+    else
+      render :new
+    end
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
